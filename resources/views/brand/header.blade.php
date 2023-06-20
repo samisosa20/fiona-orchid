6d9e36e3753cafcfc8791b00201524ecd9a5c110
Sammy Guttman <sguttman@serempre.com>
2023-06-20 05:11:57 -0500

    feat: migrate orchid dependece v14

--- resources/views/brand/header.blade.php
+++ resources/views/brand/header.blade.php
@@ -1,4 +1,7 @@
 
 <p class="h2 n-m font-thin v-center gap-3 header-logo justify-content-center">
-    <img src="/img/logo.png" alt="Fiona App" width="204" height="55"/>
+    <picture>
+        <source srcset="/img/logo.png" media="(min-width:1200px)" width="204" height="55"  />
+        <img src="/img/logo.png" alt="Fiona App" width="150" height="auto"/>
+    </picture>
 </p>
--- resources/views/layouts/account/list.blade.php
+++ resources/views/layouts/account/list.blade.php
@@ -6,7 +6,7 @@
     @endphp
     <div class="col mb-4">
         <a href="/accounts/{{$account->id}}/movements" data-turbo="true">
-            <div class="rounded shadow bg-white p-3 mx-auto cursor" style="width: 315px; height: 160px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
+            <div class="rounded shadow-sm bg-white p-3 mx-auto cursor" style="width: 350px; height: 160px;">
                 <div class="d-flex justify-content-between align-items-center">
                     <h2 class="fw-bold lh-md" style="font-size: 24px;">
                         {{$account->name}}
@@ -31,7 +31,7 @@
             }, $event->balance->toArray()));
         @endphp
         <div class="col mb-4">
-            <div class="rounded shadow bg-white p-3 mx-auto cursor" style="width: 315px; height: 135px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
+            <div class="rounded shadow-sm bg-white p-3 mx-auto cursor" style="width: 350px; height: 160px;">
                 <div class="d-flex justify-content-between align-items-center"  formaction="{{request()->server('HTTP_HOST').'/'.request()->path()}}/?event_id={{$event->id}}" data-controller="modal-toggle" data-action="click->modal-toggle#targetModal" data-modal-toggle-title="" data-modal-toggle-key="movementsModal" data-modal-toggle-async="true" data-modal-toggle-params="{&quot;event_id&quot;:{{$event->id}}}" data-modal-toggle-action="{{request()->server('HTTP_HOST').'/'.request()->path()}}/?event_id={{$event->id}}" data-modal-toggle-open="">
                     <h2 class="fw-bold lh-md" style="font-size: 24px;">
                         {{$event->name}}
@@ -56,7 +56,7 @@
     @isset($investments)
     @foreach ($investments as $investment)
         <div class="col mb-4">
-            <div class="rounded shadow bg-white p-3 mx-auto cursor" style="width: 315px; height: 135px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
+            <div class="rounded shadow-sm bg-white p-3 mx-auto cursor" style="width: 350px; height: 160px;">
                 <div class="d-flex justify-content-between align-items-center">
                     <h2 class="fw-bold lh-md" formaction="{{request()->server('HTTP_HOST').'/'.request()->path()}}/?investment_id={{$investment->id}}" data-controller="modal-toggle" data-action="click->modal-toggle#targetModal" data-modal-toggle-title="" data-modal-toggle-key="movementsModal" data-modal-toggle-async="true" data-modal-toggle-params="{&quot;investment_id&quot;:{{$investment->id}}}" data-modal-toggle-action="{{request()->server('HTTP_HOST').'/'.request()->path()}}/?investment_id={{$investment->id}}" data-modal-toggle-open="" style="font-size: 24px;">
                         {{$investment->name}}
